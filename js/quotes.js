9313e652407f3f70f3f6ea6e5a9ca2ea76eaac12
wisdom1104 <98861355+wisdom1104@users.noreply.github.com>
2023-01-04 01:04:20 +0900

    Quotes And Background (Quotes)

---
+++ js/quotes.js
@@ -0,0 +1,30 @@
+const quotes = [
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    quote:
+      "The world is a book and those who do not travel read only one page.",
+    author: "Saint Augustine",
+  },
+  {
+    quote: "Life is either a daring adventure or nothing at all.",
+    author: "Helen Keller",
+  },
+  {
+    quote: "To Travel is to Live",
+    author: "Hans Christian Andersen",
+  },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
