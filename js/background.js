ba809ef190039183bccc1fbdd5d14634e7eae246
wisdom1104 <98861355+wisdom1104@users.noreply.github.com>
2023-01-26 02:08:09 +0900

    css작업까지 마무리. ( 첫 완성)

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const namebox = document.querySelector(".name-box");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -8,6 +9,7 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
@@ -16,12 +18,15 @@ function onLoginSubmit(event) {
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  namebox.classList.add(HIDNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+const HIDNAME = "hidname";
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
+  namebox.classList.remove(HIDNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
