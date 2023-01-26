03361cb24f335c9a53111298762d0d9086690417
wisdom1104 <98861355+wisdom1104@users.noreply.github.com>
2023-01-27 01:48:56 +0900

    오류 수정본

--- js/greetings.js
+++ js/greetings.js
@@ -1,7 +1,6 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-const namebox = document.querySelector(".name-box");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -18,15 +17,12 @@ function onLoginSubmit(event) {
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
-  namebox.classList.add(HIDNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-const HIDNAME = "hidname";
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
-  namebox.classList.remove(HIDNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
